07ba5991f27e731ea71a29d1c1cd78ba617578b8
aebisberg <68967200+aebisberg@users.noreply.github.com>
2021-08-17 10:33:26 -0400

    Add files via upload

--- interactive_chat.py
+++ interactive_chat.py
@@ -1,23 +1,25 @@
 import sys
 
 import naive_bayes_bot
+import neural_net_bot
 
-import decision_tree_bot
+import logging
+logging.getLogger("tensorflow").setLevel(logging.WARNING)
+import tensorflow as tf
+
 
 chat_bot_usage = "To run the chat bot please input program name and desired bot type: " \
-                 "--decisiontree --naivebayes --neuralnet or --markovchain"
+                 "--seq2seq --naivebayes --neuralnet or --markovchain"
 
 def check_arguments():
     n = len(sys.argv)
     if n < 2:
         print(chat_bot_usage)
         exit()
-    if sys.argv[1] == "--decisiontree":
-        return decision_tree_bot.DecisionTreeBot()
     if sys.argv[1] == "--naivebayes":
         return naive_bayes_bot.NaiveBayesBot()
     if sys.argv[1] == "--neuralnet":
-        return #neural net bot
+        return neural_net_bot.NeuralNetBot()
     if sys.argv[1] == "--markovchain":
         return #markov chain bot
     else:
