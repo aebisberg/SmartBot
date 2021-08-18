7761080c7f4296076c37754f7dc5e45a6891cdb5
aebisberg <68967200+aebisberg@users.noreply.github.com>
2021-08-18 11:35:21 -0400

    Update naive_bayes_bot.py

--- naive_bayes_bot.py
+++ naive_bayes_bot.py
@@ -2,38 +2,82 @@
 #using training data and answers given to the bot
 #
 #Use formulate_response(input_text) to recieve a response to a query
-
+#
+#Author Adrian Bisberg
+import json
 import re
-import string
 from collections import defaultdict
+import numpy as np
 
 class NaiveBayesBot:
 
     def __init__(self):
         #Set up data for training
         queries, stop_words = {}, set()
+        responses = {}
+
+        with open('chatbot_data.json') as file:
+            data = json.load(file)
+
         for line in open('stop_words.txt'):
             words = line.split()
             if not re.match('^#',words[0]):
                 stop_words.add(words[0])
-        for line in open('naive_training_data.txt'):
-            contents = line.strip().split(" ")
-            label = contents.pop(len(contents)-1)
-            helpful_words = list(set(contents) - stop_words)  #remove common stop words from the list
-            if label not in queries.keys():
-                queries[label] = [helpful_words]
-            else:
-                queries[label].append(helpful_words)
-        answers = []
-        for line in open('naive_responses.txt'):
-            answers.append(line)
+
+        for intention in data['intents']:
+            for pattern in intention['patterns']:
+                pattern = re.sub(r'[^\w\s]', '', pattern)
+                contents = pattern.lower().strip().split(" ")
+                label = intention['tag']
+
+                usable_words = list(set(contents) - stop_words)  #remove common stop words from the list
+
+                if label not in queries.keys():
+                    queries[label] = [usable_words]
+                else:
+                    queries[label].append(usable_words)
+            responses[intention['tag']] = intention['responses']
+
         output = self.__prepare_data__(queries)
 
         self.trained_words = output[0]
         self.total_counts = output[1]
         self.class_counts = output[2]
-        self.prior_probs = output[3]
-        self.answers = answers
+        self.answers = responses
+
+        #Print testing results
+        self.test_chat('test_chats.json')
+
+    def test_chat(self, test_file):
+        with open(test_file) as file:
+            data = json.load(file)
+
+        test_inputs = {}
+        responses = {}
+
+        correct = 0
+        total = 0
+
+        for intention in data['intents']:
+            for pattern in intention['inputs']:
+                if intention['tag'] not in test_inputs.keys():
+                    test_inputs[intention['tag']] = [pattern]
+                if intention['tag'] not in responses.keys():
+                    responses[intention['tag']] = [pattern]
+                else:
+                    test_inputs[intention['tag']].append(pattern)
+                    for response in intention['responses']:
+                        responses[intention['tag']].append(response)
+
+        for key in test_inputs.keys():
+            for sentence in test_inputs[key]:
+                total += 1
+                answer = self.formulate_response(sentence)
+                if responses[key].__contains__(answer):
+                    correct += 1
+
+        print("Naive Bayes correctly predicted: ", correct, " out of ", total, " test seneteces")
+
 
     #get relevent counts for words across classes, total appearence of words, and total words in classes
     def __prepare_data__(self, queries):
@@ -52,48 +96,51 @@ class NaiveBayesBot:
                         counts[word] += 1
                         total_counts[word] += 1
             class_counts[key] = total_class, counts
-        prior_class_probs = {}
-        for key in class_counts.keys():
-            prior_class_probs[key] = class_counts[key][0] / total_words
-        return total_words,total_counts,class_counts, prior_class_probs
+        return total_words,total_counts,class_counts
 
     #calculates the bayes distribution of a given word
-    def __bayes_distribution__(self, given_word):
+    def __bayes_distribution__(self, sentence):
         count_dict, p_of_class = self.class_counts, defaultdict()
         for class_name in count_dict.keys():
-            p_class_given_word, p_class = 0, self.prior_probs[class_name]
-            if given_word in count_dict[class_name][1].keys():
-                p_word_given_class = count_dict[class_name][1].get(given_word) / count_dict[class_name][0]
-                p_word = self.total_counts.get(given_word)
-                p_class_given_word = (p_word_given_class * p_class) / p_word
-            p_of_class[class_name] = p_class_given_word
+            for word in sentence:
+                count = self.class_counts[class_name][1].get(word)
+                if count != None:
+                    prob_word_given_class = count / self.total_counts[word]
+                    if class_name in p_of_class.keys():
+                        p_of_class[class_name] *= prob_word_given_class
+                    else:
+                        p_of_class[class_name] = prob_word_given_class
+            if class_name in p_of_class.keys():
+                p_of_class[class_name] *= (self.class_counts[class_name][0] / self.trained_words)
         return p_of_class
 
     def formulate_response(self, input_text):
-        input_text.translate(str.maketrans('', '', string.punctuation))
+
+        input_text = re.sub(r'[^\w\s]', '', input_text)
         split_text = list(input_text.strip().split(" "))
         cumulative = defaultdict()
 
-        for word in split_text:
-            #get the bayes distribution of each word
-            answer_probs = self.__bayes_distribution__(word)
-            #accumulate the probabilities for each word
-            for key in answer_probs:
-                if key not in cumulative.keys():
+        #get the bayes distribution of each word
+        answer_probs = self.__bayes_distribution__(split_text)
+
+        #accumulate the probabilities for each word
+        for key in answer_probs:
+            if key not in cumulative.keys():
+                cumulative[key] = answer_probs[key]
+            else:
+                if cumulative[key] == 0.0:
                     cumulative[key] = answer_probs[key]
                 else:
-                    if cumulative[key] == 0.0:
-                        cumulative[key] = answer_probs[key]
-                    else:
-                        cumulative[key] = cumulative[key] * answer_probs[key]
-        best = 0
+                    cumulative[key] = cumulative[key] * answer_probs[key]
+        best_val = 0
+        best = None
 
         for key in cumulative: #searching for the maximum key
-            if cumulative[key] > best:
-                best = int(key)
-        if best == 0:
-            return self.answers.__getitem__(len(self.answers)-1) #no good match to ask again
-        return self.answers.__getitem__(best-1) #answer with the best match
-
+            if cumulative[key] > best_val:
+                best = key
+                best_val = cumulative[key]
+        if best == None:
+            return "I'm sorry I don't understand, please ask again"
+        return np.random.choice(self.answers.__getitem__(best)) #answer with the best match
 
 
