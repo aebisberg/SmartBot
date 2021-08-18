14925226f8691c43852beabbe30f99ec62d56f73
aebisberg <68967200+aebisberg@users.noreply.github.com>
2021-08-18 11:38:34 -0400

    Add files via upload

--- interactive_chat.py
+++ interactive_chat.py
@@ -1,25 +1,28 @@
+#EntryPoint for the chat bot, prompt the user to write messages to the bot
+#and continuously answer based on the appropriate model's prediction
+#
+#Author Adrian Bisberg
+
 import sys
-
 import naive_bayes_bot
-
-import decision_tree_bot
+import neural_net_bot
 
 chat_bot_usage = "To run the chat bot please input program name and desired bot type: " \
-                 "--decisiontree --naivebayes --neuralnet or --markovchain"
+                 "--neuralnet1 --naivebayes --neuralnet2 or --randomforest"
 
 def check_arguments():
     n = len(sys.argv)
     if n < 2:
         print(chat_bot_usage)
         exit()
-    if sys.argv[1] == "--decisiontree":
-        return decision_tree_bot.DecisionTreeBot()
     if sys.argv[1] == "--naivebayes":
         return naive_bayes_bot.NaiveBayesBot()
-    if sys.argv[1] == "--neuralnet":
-        return #neural net bot
-    if sys.argv[1] == "--markovchain":
-        return #markov chain bot
+    if sys.argv[1] == "--neuralnet1":
+        return neural_net_bot.NeuralNetBot()
+    if sys.argv[1] == "--neuralnet2":
+        return
+    if sys.argv[1] == "--randomforest":
+        return
     else:
         print(chat_bot_usage)
         exit()
@@ -31,7 +34,6 @@ def run_chat():
         if user_message == "quit" or user_message == "exit":
             break
         print(bot.formulate_response(user_message))
-
 
 if __name__ == '__main__':
     run_chat()
